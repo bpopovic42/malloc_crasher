fc5ae9450f05fced8b5478b3e3033d1e10faf9ee
boby <bpopovi42@gmail.com>
2021-04-19 21:36:23 +0200

    Finished re-implementing display

--- malloc_crasher.py
+++ malloc_crasher.py
@@ -20,6 +20,79 @@ class Flags():
         self.output_destination = None
 
 
+def print_initial_info(content: str, position: int, total_calls: int, call_count: int):
+    malloc: str = content[position: position + 10]
+    l1: str = " {}TARGET CODE{} ".format(COLOR_GRN, COLOR_END)
+    l2: str = " {}malloc call {}{}/{}{}".format(COLOR_YLW, COLOR_GRN, call_count, total_calls, COLOR_END)
+    l3: str = "... " + content[position - 38: position - 9]
+    l4: str = (
+            content[position - 9: position] +
+            "{}[{}]{}".format(COLOR_GRN, content[position: position + 10], COLOR_END) +
+            content[position + 10: position + 10 + 12]
+    )
+    l5: str = content[position + 10 + 12: position + 51] + " ..."
+    l6: str = " {}[call malloc] {}{}".format(COLOR_GRN, " ".join([malloc[i:i + 2] for i in range(0, len(malloc), 2)]),
+                                             COLOR_END)
+    print(" " + l1.center(37 + COLOR_SIZE * 2, "-") + " ")
+    print("| " + l2.center(35 + COLOR_SIZE * 3, " ") + " |")
+    print("| " + l3.center(35, " ") + " |")
+    print("| " + l4.center(35 + COLOR_SIZE * 2, " ") + " |")
+    print("| " + l5.center(35, " ") + " |")
+    print("| " + l6.ljust(35 + COLOR_SIZE * 2, " ") + " |")
+    print(" " + "".center(37, "-"))
+
+
+def print_infection(content: str, position: int, total_calls: int, calls_count: int):
+    original_malloc: str = content[position: position + 10]
+    l1: str = " {}POISONED CODE{} ".format(COLOR_RED, COLOR_END)
+    l2: str = " {}malloc call {}{}/{}{}".format(COLOR_YLW, COLOR_GRN, calls_count, total_calls, COLOR_END)
+    l3: str = "... " + content[position - 38: position - 9]
+    l4: str = (
+            content[position - 9: position] +
+            "{}[4831C0][90][90]{}".format(COLOR_RED, COLOR_END) +
+            content[position + 10: position + 10 + 8]
+    )
+    l5: str = content[position + 10 + 8: position + 47] + " ..."
+    l6: str = " {}+ [xor rax, rax] 48 31 C0{}".format(COLOR_RED, COLOR_END)
+    l7: str = " {}+ [nop]          90{}".format(COLOR_RED, COLOR_END)
+    l8: str = " {}- [call malloc]  {}{}".format(COLOR_GRN, " ".join(
+        [original_malloc[i:i + 2] for i in range(0, len(original_malloc), 2)]), COLOR_END)
+    print(" " + l1.center(37 + COLOR_SIZE * 2, "-") + " ")
+    print("| " + l2.center(35 + COLOR_SIZE * 3, " ") + " |")
+    print("| " + l3.center(35, " ") + " |")
+    print("| " + l4.center(35 + COLOR_SIZE * 2, " ") + " |")
+    print("| " + l5.center(35, " ") + " |")
+    print("| " + l6.ljust(35 + COLOR_SIZE * 2, " ") + " |")
+    print("| " + l7.ljust(35 + COLOR_SIZE * 2, " ") + " |")
+    print("| " + l7.ljust(35 + COLOR_SIZE * 2, " ") + " |")
+    print("| " + l8.ljust(35 + COLOR_SIZE * 2, " ") + " |")
+    print(" " + "".center(37, "-"))
+
+
+def prompt_user_to_proceed(infected_binary_name: str):
+    print(COLOR_GRN + ">> ENTER TO RUN <<".center(35, " ") + COLOR_END)
+    input("")
+    print(COLOR_YLW + ("-> Running " + infected_binary_name + " with vulnerable code...\n") + COLOR_END)
+
+
+def print_execution_info(infected_binary_process: subprocess.CompletedProcess, total_nbr_of_calls: int, crash_count: int):
+    infected_binary_stdout: str = infected_binary_process.stdout.decode("utf-8")
+    infected_binary_stderr: str = infected_binary_process.stderr.decode("utf-8")
+    if infected_binary_stdout == "" and infected_binary_stderr == "":
+        print(COLOR_YLW + "Target output is empty\n" + COLOR_END)
+    else:
+        print(COLOR_YLW + "Target output :" + COLOR_END)
+        if infected_binary_stdout != "": print(infected_binary_stdout)
+        if infected_binary_stderr != "": print(infected_binary_stderr)
+    print(COLOR_YLW + "CRASH : " + COLOR_END, end="")
+    if infected_binary_process.returncode < 0:
+        print(COLOR_RED + "YES" + (" (SEGFAULT)" + COLOR_END) if infected_binary_process.returncode == -11 else COLOR_END)
+    else:
+        print(COLOR_GRN + "NO" + COLOR_END)
+    print(COLOR_GRN + "Crash count : {}/{}".format(crash_count, total_nbr_of_calls) + COLOR_END)
+    input("")
+
+
 def get_opcodes(malloc_calls: List[str]) -> List[str]:
     opcodes = []
     for call in malloc_calls:
@@ -39,7 +112,6 @@ def get_malloc_opcodes(target_program: str) -> List[str]:
 
 
 def parse_program_args(program_args: List[str]) -> Tuple[str, List[str]]:
-    print(program_args)
     target_program = program_args[1]
     parameters = []
     return target_program, parameters
@@ -49,40 +121,12 @@ def get_target_program_content(target_program: str) -> str:
     with open(target_program, "rb") as f:
         content = f.read().hex()
         f.close()
-    #for i in range(0, len(content), 1024):
-        #print(content[i:i+1024])
     return content
 
 
-def print_initial_info(infected_binary_name: str, content: str, position: int, infected_opcode: str):
-    malloc = content[position : position + 10]
-    l1  = " {}TARGET CODE{} ".format(COLOR_GRN, COLOR_END)
-    l2  = " {}malloc call {}{}/{}{}".format(COLOR_YLW, COLOR_GRN, 1, 2, COLOR_END)
-    l3  = "... " + content[position - 38: position - 9]
-    l4  = (
-            content[position - 9 : position] +
-            "{}[{}]{}".format(COLOR_GRN, content[position : position + 10], COLOR_END) +
-            content[position + 10 : position + 10 + 12]
-    )
-    l5 = content[position + 10 + 12 : position + 51] + " ..."
-    l6 = " {}[call malloc] {}{}".format(COLOR_GRN, " ".join([malloc[i:i+2] for i in range(0, len(malloc), 2)]), COLOR_END)
-    print(" " + l1.center(37 + COLOR_SIZE * 2, "-") + " ")
-    print("| " + l2.center(35 + COLOR_SIZE * 3, " ") + " |")
-    print("| " + l3.center(35, " ") + " |")
-    print("| " + l4.center(35 + COLOR_SIZE * 2, " ") + " |")
-    print("| " + l5.center(35, " ") + " |")
-    print("| " + l6.ljust(35 + COLOR_SIZE * 2, " ") + " |")
-    #print(COLOR_YLW + ">> " + infected_binary_name + " <<")
-
-
-def prompt_user_to_proceed(infected_binary_name: str):
-    print(COLOR_GRN + ">> ENTER TO RUN <<" + COLOR_END)
-    input("")
-    print(COLOR_YLW + "-> Running " + infected_binary_name + " with vulnerable code...\n")
-
-
-def create_infected_binary(infected_binary_name: str, source_binary_content: str, opcode_position: int, opcode_length: int):
-    infected_opcode: str = "4831c09090" # Xor RAX register + 2 NO-OPS to override total malloc opcode length
+def create_infected_binary(infected_binary_name: str, source_binary_content: str, opcode_position: int,
+                           opcode_length: int):
+    infected_opcode: str = "4831c09090"  # Xor RAX register + 2 NO-OPS to override total malloc opcode length
     infected_content = (
             source_binary_content[:opcode_position]
             + infected_opcode
@@ -95,45 +139,33 @@ def create_infected_binary(infected_binary_name: str, source_binary_content: str
     os.chmod(infected_binary_name, 0o0777)
 
 
-def print_execution_info(infected_binary_exit_code: int):
-    print(COLOR_YLW + "CRASH : " + (COLOR_GRN + "NO" if infected_binary_exit_code >= 0 else COLOR_RED + "YES"))
-    if infected_binary_exit_code == -11:
-        print(COLOR_RED + "(SEGFAULT)")
-
-
-def run_infected_binary(flags: Flags, infected_binary_name: str) -> int:
-    crash: int = 0
-    exit_code: int = subprocess.call("./" + infected_binary_name)
-    if exit_code < 0:
-        crash = 1
-    if flags.has_interactive:
-        print_execution_info(exit_code)
-    return crash
-
-
-def print_crash_count(crash_count: int, total_nbr_of_executions: int):
-    print(COLOR_YLW + "crash count : " + COLOR_RED + str(crash_count) + "/" + str(total_nbr_of_executions) + COLOR_END)
-    input("")
+def run_infected_binary(flags: Flags, infected_binary_name: str) -> subprocess.CompletedProcess:
+    infected_binary_process: subprocess.CompletedProcess = subprocess.run(["./" + infected_binary_name], capture_output=True)
+    return infected_binary_process
 
 
 def main():
-    crash_count = 0
+    crash_count: int = 0
+    calls_count: int = 1
     flags = Flags()
     flags.has_interactive = True
     target_program, parameters = parse_program_args(sys.argv)
     malloc_opcodes = get_malloc_opcodes(target_program)
     source_binary_content: str = get_target_program_content(target_program)
     for opcode in malloc_opcodes:
-        print(opcode)
-        opcode_position = source_binary_content.find(opcode) # TODO: determine the reason for this 2 bytes offset
-        print(opcode_position)
+        os.system('clear')
+        opcode_position = source_binary_content.find(opcode)
         infected_binary_name: str = target_program + ".infected"
         create_infected_binary(infected_binary_name, source_binary_content, opcode_position, len(opcode))
         if flags.has_interactive:
-            print_initial_info(infected_binary_name, source_binary_content, opcode_position, "4831c09090")
+            print_initial_info(source_binary_content, opcode_position, len(malloc_opcodes), calls_count)
+            print_infection(source_binary_content, opcode_position, len(malloc_opcodes), calls_count)
             prompt_user_to_proceed(infected_binary_name)
-        crash_count += run_infected_binary(flags, infected_binary_name)
-        print_crash_count(crash_count, len(malloc_opcodes))
+        infected_binary_process: subprocess.CompletedProcess = run_infected_binary(flags, infected_binary_name)
+        crash_count += 1 if infected_binary_process.returncode < 0 else 0
+        if flags.has_interactive:
+            print_execution_info(infected_binary_process, len(malloc_opcodes), crash_count)
+        calls_count += 1
 
 
 if __name__ == '__main__':
